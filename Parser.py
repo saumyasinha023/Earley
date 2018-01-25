6883e32dd84f42f45ebf89efb5de3273ab4ba06b
Minal2179 <minalshettigar2971@gmail.com>
2018-01-25 13:14:21 -0800

    Stemmer code merged

--- Parser.py
+++ Parser.py
@@ -1,20 +1,84 @@
-import re
 
-pos = ["Verb", "Aux", "Det", "Proper-Noun", "Pronoun", "Noun", "Prep"]
-dict = {"S": ["NP VP", "Aux NP VP", "VP"],
-        "NP": ["Pronoun", "Proper-Noun", "Det Nominal"],
-        "VP": ["Verb", "Verb NP", "Verb NP PP", "Verb PP", "VP PP"],
-        "Aux": ["can", "will"],
-        "Det": ["the", "that"],
-        "Pronoun": ["he", "she"],
-        "Proper-Noun": ["mary", "john"],
-        "Nominal": ["Noun", "Nominal Noun", "Nominal PP"],
-        "Noun": ["book", "flight"],
-        "Verb": ["do", "work", "book"],
-        "PP": ["Prep NP"],
-        "Prep": ["in", "on", "at"],
-        "W": ["book", "that", "flight"]
-        }
+import re
+import atexit
+import fileinput
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+from collections import defaultdict
+
+ps = PorterStemmer()
+
+@atexit.register
+def quit_gracefully():
+    print("ENDFILE")
+
+def error_quit():
+    print("input is erroneous")
+
+def findType(inputToken):
+    if(re.match('^[-+]?[0-9]+$', inputToken)):
+        return "INT"
+    elif(re.match('[+-]?([0-9]*[.])?[0-9]+',inputToken)):
+        return "DOUBLE"
+    elif(re.match('\W',inputToken)):
+        return "OP"
+    elif (type('\w') is str):
+        return "STRING"
+
+print("Stemmer: ")
+grammar = defaultdict(list)
+partofspeech = set()
+for line in fileinput.input():
+    if re.match('[^(a-zA-Z|\*|\s|\:|\=|\;|\-|\|)]',line):
+        atexit.register(error_quit())
+
+    parts = re.match('([a-zA-Z\-]+)\s*[\:|\=]\s*([a-zA-Z\-\|\*\s*]*)', line)
+    LHS = parts.group(1)
+
+    if parts.group(2).find('|') == -1 and LHS == 'W':
+        subpart = re.split('\s+', parts.group(2))
+    else:
+        subpart = re.split('\s*\|\s*', parts.group(2))
+
+    for i in range(len(subpart)):
+        if subpart[i].islower():
+            if LHS != 'W':
+                partofspeech.add(LHS)
+        if LHS == 'W':
+            RHS = ps.stem(subpart[i])
+        else:
+            RHS = subpart[i]
+        grammar[LHS].append(RHS.rstrip())
+
+    for w in word_tokenize(line):
+        type_of_word = findType(w)
+        if line.find('=') == -1:
+            print(w, ' ', type_of_word, ' ', fileinput.lineno())
+        else:
+            if re.match('[A-Za-z]',w) and w != "W":
+                print(w,' ',type_of_word,' ',fileinput.lineno(),' ',ps.stem(w),' ')
+            else:
+                print(w, ' ', type_of_word, ' ', fileinput.lineno())
+
+for x in grammar:
+    print (x,' : ',grammar[x])
+print('partofspeech: ',partofspeech)
+
+# pos = ["Verb", "Aux", "Det", "Proper-Noun", "Pronoun", "Noun", "Prep"]
+# dict = {"S": ["NP VP", "Aux NP VP", "VP"],
+#         "NP": ["Pronoun", "Proper-Noun", "Det Nominal"],
+#         "VP": ["Verb", "Verb NP", "Verb NP PP", "Verb PP", "VP PP"],
+#         "Aux": ["can", "will"],
+#         "Det": ["the", "that"],
+#         "Pronoun": ["he", "she"],
+#         "Proper-Noun": ["mary", "john"],
+#         "Nominal": ["Noun", "Nominal Noun", "Nominal PP"],
+#         "Noun": ["book", "flight"],
+#         "Verb": ["do", "work", "book"],
+#         "PP": ["Prep NP"],
+#         "Prep": ["in", "on", "at"],
+#         "W": ["book", "that", "flight"]
+#         }
 
 
 def extractAfterStarState(string):
@@ -26,12 +90,11 @@ def extractAfterStarState(string):
         return 0
 
 
-def predictor(row_elem, i):
+def predictor(row_elem):
     RHS = extractAfterStarState(row_elem[0])
     LHS = find_LHS(row_elem[0])
-    if LHS not in pos:
-        for each in dict[RHS]:
-            # if each not in pos:
+    if LHS not in partofspeech:
+        for each in grammar[RHS]:
             toAppend = [RHS + " -> " + " * " + each, row_elem[2], row_elem[2], "predictor"]
             if toAppend not in s[row_elem[2]]:
                 enqueue(row_elem[2], toAppend)
@@ -39,20 +102,20 @@ def predictor(row_elem, i):
 
 def scanner(row_elem, i):
     fetched = extractAfterStarState(row_elem[0])
-    if i < len(dict["W"]) and dict["W"][i] in dict[fetched]:
-        toAppend = [fetched + " -> " + dict["W"][i] + " * ", row_elem[2], row_elem[2] + 1, "scanner"]
+    if i < len(grammar["W"]) and grammar["W"][i] in grammar[fetched]:
+        toAppend = [fetched + " -> " + grammar["W"][i] + " * ", row_elem[2], row_elem[2] + 1, "scanner"]
         enqueue(row_elem[2] + 1, toAppend)
 
 
-def completer(row_elem, i):
+def completer(row_elem):
     LHS = find_LHS(row_elem[0])
     for each in s[row_elem[1]]:
         if ("*" + " " + LHS) in each[0] and LHS != "S":
             left = each[0].split('*')[0]
             right = each[0].split('*')[1]
             mid = right.split(" ")[1]
-            toAppend = [left + mid + " * " + " ".join(right.split()[1:]), row_elem[1], row_elem[2], "completer"]
-            enqueue(row_elem[1] + 1, toAppend)
+            toAppend = [left + mid + " * " + " ".join(right.split()[1:]), each[1], row_elem[2], "completer"]
+            enqueue(row_elem[2], toAppend)
 
 
 def find_LHS(string):
@@ -71,16 +134,12 @@ def Earley_parser(words):
     for i in range(len(words) + 1):
         for row_elem in s[i]:
             RHS = extractAfterStarState(row_elem[0])
-            # if RHS==0 and RHS not in dict:
-            #     continue
-            # if s[i].index(row_elem) == 0 and RHS == 0:
-            #     continue
-            if RHS != 0 and RHS not in pos:
-                predictor(row_elem, i)
-            elif RHS != 0 and RHS in pos:
+            if RHS != 0 and RHS not in partofspeech:
+                predictor(row_elem)
+            elif RHS != 0 and RHS in partofspeech:
                 scanner(row_elem, i)
             else:
-                completer(row_elem, i)
+                completer(row_elem)
 
 
 def enqueue(state, chart_entry):
@@ -93,17 +152,13 @@ def enqueue(state, chart_entry):
         s[state].append(chart_entry)
 
 
-def checkNextState(string, state):
-    pass
-
-
 def initialize(words):
     global s
     s = [[] for i in range(len(words) + 1)]
-    Earley_parser(dict["W"])
+    Earley_parser(grammar["W"])
 
 
-initialize(dict["W"])
+initialize(grammar["W"])
 for each in s:
     for elem in each:
         print(elem)
